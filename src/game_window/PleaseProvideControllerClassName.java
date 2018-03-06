6b241f63336187c893b4e2d5755822754a86c030
Pplociennik <przemx.84@gmail.com>
2018-03-06 11:58:11 +0100

    ...

---
+++ src/game_window/PleaseProvideControllerClassName.java
@@ -0,0 +1,16 @@
+package game_window;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+import static improvement.TownHall_improvementImpl.improveTownHall;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    void clickButton(MouseEvent event) {
+        improveTownHall();
+
+    }
+
+}
