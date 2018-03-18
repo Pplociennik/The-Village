4551c5d44a8d770882716a7258765880b090931e
Pplociennik <przemx.84@gmail.com>
2018-03-18 22:31:44 +0100

    .

--- src/main/java/buildings/Storage.java
+++ src/main/java/buildings/Storage.java
@@ -5,7 +5,7 @@ public class Storage {
     double food = 2.75;
     final double maxFood = 3.75;
 
-    int wood = 750;
+    int wood = 1000;
     final int maxWood = 1000;
 
 
@@ -14,15 +14,15 @@ public class Storage {
         return food;
     }
 
-    public void addFood(double food) {
-        this.food = food + this.food;
+    public void addFood(double foodIncome) {
+        this.food = food + foodIncome;
     }
 
     public int getWood() {
         return wood;
     }
 
-    public void addWood(int wood) {
-        this.wood = wood + this.wood;
+    public void addWood(int woodIncome) {
+        this.wood = wood + woodIncome;
     }
 }
--- src/main/java/buildings/TownHall.java
+++ src/main/java/buildings/TownHall.java
@@ -6,8 +6,8 @@ public class TownHall {
     int level = 1;
     final int maxLevel = 30;
 
-    final int[] woodForLvl = {0, 50, 0, 20, 300, 4, 50, 60, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
-    final double[] foodForLvl = {0, 0.20, 0, 20, 300, 4, 50, 60, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+    final int[] woodForLvl = {0, 50, 75, 126, 300, 450, 500, 60, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+    final double[] foodForLvl = {0, 0.20, 0.20, 0.35, 0.40, 0.40, 50, 60, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
 
 
     //methods
@@ -16,8 +16,15 @@ public class TownHall {
             if (storage.getFood() >= foodForLvl[level]) {
                 if(storage.getWood() >= woodForLvl[level]) {
                     level++; storage.addWood(-woodForLvl[level]); storage.addFood(-foodForLvl[level]);
+                    System.out.println("Town Hall Improved!");
+                    System.out.println("Town Hall Level: "+level);
+                    System.out.println("Wood: "+storage.getWood());
+                    System.out.println("Food: "+storage.getFood());
                 }
+                else {System.out.println("Too little wood!");}
             }
+            else {System.out.println("Too little food!");}
         }
+        else {System.out.println("Max levelof Town Hall!");}
     }
 }
