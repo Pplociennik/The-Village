c961c7245c55fbc020215375dadf97a3a3bf17c2
pszemko <przemx.84@gmail.com>
2018-03-19 20:50:43 +0100

    Added new materials

--- src/main/java/buildings/Storage.java
+++ src/main/java/buildings/Storage.java
@@ -2,27 +2,273 @@ package buildings;
 
 public class Storage {
     //attributes
-    double food = 2.75;
-    final double maxFood = 3.75;
+    double food = 2000;
+    double maxFood = 3.75;
 
-    int wood = 1000;
-    final int maxWood = 1000;
 
+    int wood = 100000;
+    int maxWood = 1000;
+
+    int sandstone = 1500;
+    int maxSandstone = 1500;
+
+    int sand = 100;
+    int maxSand = 100;
+
+    int glass = 100;
+    int maxGlass = 100;
+
+    int coal = 25;
+    int maxCoal = 25;
+
+    int iron = 10;
+    int maxIron = 100;
+
+    int gold = 6;
+    int maxGold  =100;
+
+    int diamond = 1;
+    int maxDiamond = 20;
+
+    int dyers = 45;
+    int maxDyers = 125;
+
+    int whiteWool = 75;
+    int maxWhiteWool = 150;
+
+    int dyedWool = 75;
+    int maxDyedWool = 150;
+
+    int cobblestone = 250;
+    int maxCobblestone = 350;
+
+    int stone = 45;
+    int maxStone = 65;
 
     //methods
+
+
     public double getFood() {
         return food;
     }
 
-    public void addFood(double foodIncome) {
-        this.food = food + foodIncome;
+    public void setFood(double food) {
+        this.food = food;
+    }
+
+    public double getMaxFood() {
+        return maxFood;
+    }
+
+    public void setMaxFood(double maxFood) {
+        this.maxFood = maxFood;
     }
 
     public int getWood() {
         return wood;
     }
 
-    public void addWood(int woodIncome) {
-        this.wood = wood + woodIncome;
+    public void setWood(int wood) {
+        this.wood = wood;
+    }
+
+    public int getMaxWood() {
+        return maxWood;
+    }
+
+    public void setMaxWood(int maxWood) {
+        this.maxWood = maxWood;
+    }
+
+    public int getSandstone() {
+        return sandstone;
+    }
+
+    public void setSandstone(int sandstone) {
+        this.sandstone = sandstone;
+    }
+
+    public int getMaxSandstone() {
+        return maxSandstone;
+    }
+
+    public void setMaxSandstone(int maxSandstone) {
+        this.maxSandstone = maxSandstone;
+    }
+
+    public int getSand() {
+        return sand;
+    }
+
+    public void setSand(int sand) {
+        this.sand = sand;
+    }
+
+    public int getMaxSand() {
+        return maxSand;
+    }
+
+    public void setMaxSand(int maxSand) {
+        this.maxSand = maxSand;
+    }
+
+    public int getGlass() {
+        return glass;
+    }
+
+    public void setGlass(int glass) {
+        this.glass = glass;
+    }
+
+    public int getMaxGlass() {
+        return maxGlass;
+    }
+
+    public void setMaxGlass(int maxGlass) {
+        this.maxGlass = maxGlass;
+    }
+
+    public int getCoal() {
+        return coal;
+    }
+
+    public void setCoal(int coal) {
+        this.coal = coal;
+    }
+
+    public int getMaxCoal() {
+        return maxCoal;
+    }
+
+    public void setMaxCoal(int maxCoal) {
+        this.maxCoal = maxCoal;
+    }
+
+    public int getIron() {
+        return iron;
+    }
+
+    public void setIron(int iron) {
+        this.iron = iron;
+    }
+
+    public int getMaxIron() {
+        return maxIron;
+    }
+
+    public void setMaxIron(int maxIron) {
+        this.maxIron = maxIron;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
+
+    public int getMaxGold() {
+        return maxGold;
+    }
+
+    public void setMaxGold(int maxGold) {
+        this.maxGold = maxGold;
+    }
+
+    public int getDiamond() {
+        return diamond;
+    }
+
+    public void setDiamond(int diamond) {
+        this.diamond = diamond;
+    }
+
+    public int getMaxDiamond() {
+        return maxDiamond;
+    }
+
+    public void setMaxDiamond(int maxDiamond) {
+        this.maxDiamond = maxDiamond;
+    }
+
+    public int getDyers() {
+        return dyers;
+    }
+
+    public void setDyers(int dyers) {
+        this.dyers = dyers;
+    }
+
+    public int getMaxDyers() {
+        return maxDyers;
+    }
+
+    public void setMaxDyers(int maxDyers) {
+        this.maxDyers = maxDyers;
+    }
+
+    public int getWhiteWool() {
+        return whiteWool;
+    }
+
+    public void setWhiteWool(int whiteWool) {
+        this.whiteWool = whiteWool;
+    }
+
+    public int getMaxWhiteWool() {
+        return maxWhiteWool;
+    }
+
+    public void setMaxWhiteWool(int maxWhiteWool) {
+        this.maxWhiteWool = maxWhiteWool;
+    }
+
+    public int getDyedWool() {
+        return dyedWool;
+    }
+
+    public void setDyedWool(int dyedWool) {
+        this.dyedWool = dyedWool;
+    }
+
+    public int getMaxDyedWool() {
+        return maxDyedWool;
+    }
+
+    public void setMaxDyedWool(int maxDyedWool) {
+        this.maxDyedWool = maxDyedWool;
+    }
+
+    public int getCobblestone() {
+        return cobblestone;
+    }
+
+    public void setCobblestone(int cobblestone) {
+        this.cobblestone = cobblestone;
+    }
+
+    public int getMaxCobblestone() {
+        return maxCobblestone;
+    }
+
+    public void setMaxCobblestone(int maxCobblestone) {
+        this.maxCobblestone = maxCobblestone;
+    }
+
+    public int getStone() {
+        return stone;
+    }
+
+    public void setStone(int stone) {
+        this.stone = stone;
+    }
+
+    public int getMaxStone() {
+        return maxStone;
+    }
+
+    public void setMaxStone(int maxStone) {
+        this.maxStone = maxStone;
     }
 }
--- src/main/java/buildings/TownHall.java
+++ src/main/java/buildings/TownHall.java
@@ -7,24 +7,73 @@ public class TownHall {
     final int maxLevel = 30;
 
     final int[] woodForLvl = {0, 50, 75, 126, 300, 450, 500, 60, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+    final int[] cobbleForLvl = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    final int[] sandstoneForLvl = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    final int[] glassForLvl = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    final int[] stoneForLvl = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    final int[] whiteWoolForLvl = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    final int[] dyedWoolForLvl = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
     final double[] foodForLvl = {0, 0.20, 0.20, 0.35, 0.40, 0.40, 50, 60, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
 
 
     //methods
     public void improve(Storage storage) {
         if (level < maxLevel) {
-            if (storage.getFood() >= foodForLvl[level]) {
-                if(storage.getWood() >= woodForLvl[level]) {
-                    level++; storage.addWood(-woodForLvl[level]); storage.addFood(-foodForLvl[level]);
-                    System.out.println("Town Hall Improved!");
-                    System.out.println("Town Hall Level: "+level);
-                    System.out.println("Wood: "+storage.getWood());
-                    System.out.println("Food: "+storage.getFood());
+            if (storage.getFood() >= foodForLvl[level - 1]) {
+                if (storage.getWood() >= woodForLvl[level - 1]) {
+                    if (storage.getCobblestone() >= cobbleForLvl[level - 1]) {
+                        if (storage.getSandstone() >= sandstoneForLvl[level - 1]) {
+                            if (storage.getGlass() >= glassForLvl[level - 1]) {
+                                if (storage.getStone() >= stoneForLvl[level - 1]) {
+                                    if (storage.getWhiteWool() >= whiteWoolForLvl[level - 1]) {
+                                        if (storage.getDyedWool() >= dyedWoolForLvl[level - 1]) {
+                                            level++;
+                                            storage.setWood((storage.getWood() - woodForLvl[level - 1]));
+                                            storage.setFood((storage.getFood() - foodForLvl[level - 1]));
+                                            storage.setCobblestone(storage.getCobblestone() - cobbleForLvl[level - 1]);
+                                            storage.setSandstone(storage.getSandstone() - sandstoneForLvl[level - 1]);
+                                            storage.setGlass(storage.getGlass() - glassForLvl[level - 1]);
+                                            storage.setStone(storage.getStone() - stoneForLvl[level - 1]);
+                                            storage.setWhiteWool(storage.getWhiteWool() - whiteWoolForLvl[level - 1]);
+                                            storage.setDyedWool(storage.getDyedWool() - dyedWoolForLvl[level - 1]);
+                                            System.out.println("Town Hall Improved!");
+                                            System.out.println("Town Hall Level: " + level);
+                                            System.out.println("Wood: " + storage.getWood());
+                                            System.out.println("Food: " + storage.getFood());
+                                            System.out.println("Cobblestone: " + storage.getCobblestone());
+                                            System.out.println("Sandstone: " + storage.getSandstone());
+                                            System.out.println("Glass: " + storage.getGlass());
+                                            System.out.println("Stone: " + storage.getStone());
+                                            System.out.println("White Wool: " + storage.getWhiteWool());
+                                            System.out.println("Dyed Wool: " + storage.getDyedWool());
+                                        } else {
+                                            System.out.println("Too little dyed wool!");
+                                        }
+                                    } else {
+                                        System.out.println("Too little white wool!");
+                                    }
+                                } else {
+                                    System.out.println("Too little stone!");
+                                }
+                            } else {
+                                System.out.println("Too little glass!");
+                            }
+                        } else {
+                            System.out.println("Too little sandstone!");
+                        }
+                    } else {
+                        System.out.println("Too little cobblestone!");
+                    }
+                } else {
+                    System.out.println("Too little wood!");
                 }
-                else {System.out.println("Too little wood!");}
+            } else {
+                System.out.println("Too little food!");
             }
-            else {System.out.println("Too little food!");}
+        } else {
+            System.out.println("Max level of Town Hall!");
         }
-        else {System.out.println("Max levelof Town Hall!");}
     }
 }
+
