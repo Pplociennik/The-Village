9f328a81fb1d3fd7890b303b9b173c1f71aec979
pszemko <przemx.84@gmail.com>
2018-03-20 14:02:33 +0100

    Added storage improving button

--- src/main/java/game_window/Controller.java
+++ src/main/java/game_window/Controller.java
@@ -6,6 +6,7 @@ package game_window;
 import buildings.Storage;
 import buildings.TownHall;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
@@ -20,12 +21,20 @@ public class Controller {
     @FXML // fx:id="townHallPane"
     private AnchorPane townHallPane; // Value injected by FXMLLoader
 
+    @FXML // fx:id="storageimpr"
+    private Button storageimpr; //Value injected by FXMLLoader
+
     @FXML // fx:id="pane2"
     private AnchorPane pane2; // Value injected by FXMLLoader
 
     @FXML
     void clickTownHallImprovement(MouseEvent event) {
         townHall.checkAndImprove(storage);
+    }
+
+    @FXML
+    void clickImproveStorage(MouseEvent event)  {
+        storage.checkAndImprove();
     }
 
     @FXML
